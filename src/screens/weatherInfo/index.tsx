7dbc9b360d9842a45b0a4f746426507d735ada01
Med Redha <redha.dirac@gmail.com>
2021-07-10 16:18:11 +0200

    🚧   add weatherInfo screen

--- src/screens/weatherInfo/index.tsx
+++ src/screens/weatherInfo/index.tsx
@@ -1,24 +1,63 @@
 import * as React from 'react';
-import {useContext} from 'react';
+import {useContext, useEffect} from 'react';
 
-import {Text, View} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import {Spinner} from 'react-native-ios-kit';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
-import LinearGradient from 'react-native-linear-gradient';
 import {ThemeContext} from '../../hooks/useTheme';
-import styles from './SearchScreen.style';
+import {getCurrentWeather} from '../../services/actions';
+import styles from './WeatherInfo.style';
 
-export default function SearchScreen() {
+const mapStateToProps = (state) => ({
+  hasCity: state.data.hasCity,
+  weather: state.data.weather,
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      isLoading: (action, value) => dispatch({type: action, value}),
+      getCurrentWeather,
+    },
+    dispatch
+  );
+
+function WeatherInfo({route, navigation, weather, getCurrentWeather}) {
   const {theme}: any = useContext(ThemeContext);
+  const {city} = route?.params;
+  navigation.setOptions({title: city});
+
+  const {isLoading, error} = weather;
+
+  useEffect(() => {
+    getCurrentWeather(city);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <SafeAreaView
+        style={{...styles.safeArea, backgroundColor: theme.primary}}>
+        <Spinner animating size='large' color={theme.gray} />
+      </SafeAreaView>
+    );
+  }
+
+  if (error) {
+    return (
+      <SafeAreaView
+        style={{...styles.safeArea, backgroundColor: theme.primary}}>
+        <Text style={{color: theme.text, fontSize: 26}}>{error}</Text>
+      </SafeAreaView>
+    );
+  }
 
   return (
-    <>
-      <LinearGradient
-        colors={theme.gradient}
-        style={{
-          ...styles.container,
-        }}>
-        <Text style={{color: theme.text, fontSize: 26}}> SearchScreen </Text>
-      </LinearGradient>
-    </>
+    <SafeAreaView style={{...styles.safeArea, backgroundColor: theme.primary}}>
+      <Text style={{color: theme.text, fontSize: 26}}> SearchScreen </Text>
+    </SafeAreaView>
   );
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(WeatherInfo);
